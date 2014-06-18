23b72f39a54a587d4b45bbbd60da1877f7a817aa
C-o-r-E <can.of.tuna@gmail.com>
2014-06-17 21:47:59 -0400

    fix another typo

--- brdcst.py
+++ brdcst.py
@@ -7,7 +7,8 @@ bcast_info = (bcast_ip, bcast_port)
 
 print "attempting to send udp broadcast message"
 
-s = socket.socket(socket.af_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 s.sendto("this is a test", bcast_info)
 

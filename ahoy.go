53c9f112e2ff893d6c041a27545a151e2bcef3e3
C-o-r-E <can.of.tuna@gmail.com>
2014-07-08 16:33:27 -0400

    basic functionality implemented

--- ahoy.go
+++ ahoy.go
@@ -10,8 +10,8 @@ import (
 func main() {
 
 	fmt.Println("Which mode do you want to run in?")
-	fmt.Println("(S)erver")
-	fmt.Println("(C)lient")
+	fmt.Println("(S)erver (i am a device)")
+	fmt.Println("(C)lient (i am an app)")
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -33,33 +33,139 @@ func main() {
 
 }
 
-func client() {
-	conn, err := net.Dial("udp", "255.255.255.255:2050")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error connecting: %s\n", err)
-	}
-	fmt.Fprintf(conn, "Testing with Go~")
+func fail(msg string, err error) {
+	fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error %s: %s\n", msg, err)
 }
 
-func server() {
+func client_handle_connection(channel chan string, connection net.Conn) {
+
+	var buf [1024]byte
+
+	//try to say hello
+	connection.Write([]byte("Ahoy?"))
+
+	n, err := connection.Read(buf[0:1023])
+	if err != nil {
+		fail("read", err)
+		return
+	}
+
+	fmt.Printf("got %d bytes ", n)
+	s := string(buf[0:n])
+	fmt.Printf("[%s]\n", s)
+
+	if s == "Ahoy!" {
+		//we have one
+		fmt.Println(connection.RemoteAddr(), "is valid")
+		channel <- connection.RemoteAddr().String()
+	}
+
+	connection.Close()
+
+}
+
+func client_listener(c chan string) {
+
+	ln, err := net.Listen("tcp", ":5020")
+	if err != nil {
+		fail("listening", err)
+		return
+	}
+
+	//fmt.Printf("now listening on port %s\n", ln.Addr().String())
+	c <- "listening"
+
+	for {
+
+		conn, err := ln.Accept()
+		if err != nil {
+			fail("waiting for connection", err)
+			return
+		}
+
+		go client_handle_connection(c, conn)
+
+	}
+
+}
+
+func client() { // you can call this the app
+
+	batphone := make(chan string)
+
+	go client_listener(batphone)
+
+	alfred := <-batphone
+
+	if alfred != "listening" {
+		fmt.Printf("Got a prank call: [%s]", alfred)
+		return
+	}
+
+	conn, err := net.Dial("udp", "255.255.255.255:2050")
+	if err != nil {
+		fail("connecting", err)
+		return
+	}
+	fmt.Fprintf(conn, "Testing with Go~")
+
+	for {
+		alfred := <-batphone
+		fmt.Printf("Alfred: [%s]", alfred)
+	}
+}
+
+func server_hello(address net.Addr) {
+	var buf [1024]byte
+
+	addr, err := net.ResolveUDPAddr("udp", address.String())
+
+	dest := fmt.Sprintf("%s:5020", addr.IP.String())
+
+	conn, err := net.Dial("tcp", dest)
+	if err != nil {
+		fail("saying hello", err)
+		return
+	}
+
+	n, err := conn.Read(buf[0:1023])
+	if err != nil {
+		fail("reading", err)
+		return
+	}
+
+	s := string(buf[0:n])
+	if s == "Ahoy?" {
+		conn.Write([]byte("Ahoy!"))
+	}
+
+	conn.Close()
+
+}
+
+func server() { // this can be called a device
 	var buf [1000]byte
 
 	addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:2050")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error resolving address: %s\n", err)
+		fail("resolving address", err)
+		return
 	}
 
 	ln, err := net.ListenUDP("udp", addr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error trying to listen: %s\n", err)
+		fail("trying to listen", err)
+		return
 	}
 
-	for true {
+	for {
 		n, sender, err := ln.ReadFrom(buf[0:128])
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error while trying to read: %s\n", err)
+			fail("trying to read", err)
+			return
 		}
 
+		go server_hello(sender)
 		fmt.Printf("%s: [%s]\n", sender, buf[0:n])
 	}
 

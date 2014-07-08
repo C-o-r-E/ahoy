34c2dcfca470bec6b6ab6a2b73fa907cbe42dd08
C-o-r-E <can.of.tuna@gmail.com>
2014-07-07 20:22:13 -0400

    implemented server functionality

--- ahoy.go
+++ ahoy.go
@@ -23,6 +23,7 @@ func main() {
 
 	if choice == "S" {
 		fmt.Println("Server")
+		server()
 	} else if choice == "C" {
 		fmt.Println("Client")
 		client()
@@ -38,4 +39,28 @@ func client() {
 		fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error connecting: %s\n", err)
 	}
 	fmt.Fprintf(conn, "Testing with Go~")
+}
+
+func server() {
+	var buf [1000]byte
+
+	addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:2050")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error resolving address: %s\n", err)
+	}
+
+	ln, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error trying to listen: %s\n", err)
+	}
+
+	for true {
+		n, sender, err := ln.ReadFrom(buf[0:128])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Danger, Will Robinson! Error while trying to read: %s\n", err)
+		}
+
+		fmt.Printf("%s: [%s]\n", sender, buf[0:n])
+	}
+
 }

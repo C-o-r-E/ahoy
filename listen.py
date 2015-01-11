8cb3e2e7355dd8278f5ccf87efec5997ea0162ac
C-o-r-E <can.of.tuna@gmail.com>
2015-01-11 00:25:53 -0500

    added timestamp to listener

--- listen.py
+++ listen.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 local_ip = "0.0.0.0"
 bcast_port = 2050
@@ -7,8 +8,11 @@ sock_info = (local_ip, bcast_port)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(sock_info)
 
+start = time.time()
 print "listening for connections..."
 
 while True:
     data, addr = s.recvfrom(1024)
-    print "%s: [%s]" % (addr, data)
+    now = time.time()
+    delta = int(now - start)
+    print "t={} {}: [{}]".format(delta, addr, data)
